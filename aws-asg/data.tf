7734cec46367bfe8f90b8452d14ec4a939ab5f96
team-1-dso <itbankdso.t2@gmail.com>
2025-02-07 16:41:27 +0900

    aws-asg Update

--- aws-asg/data.tf
+++ aws-asg/data.tf
@@ -27,3 +27,13 @@ data "terraform_remote_state" "rds_remote_data" {
     region  = "ap-northeast-2"
   }
 }
+
+data "terraform_remote_state" "alb_remote_data" {
+  backend = "s3"
+  config = {
+    bucket  = "myterraform-bucket-state-jung-s"
+    key     = "${var.name}/alb/terraform.tfstate" # (Update)
+    profile = "terraform_user"
+    region  = "ap-northeast-2"
+  }
+}
